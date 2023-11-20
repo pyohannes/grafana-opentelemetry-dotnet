efd0f9d9493bd015afda0022f823a2acab592dd5
Johannes Tax <johannes.tax@grafana.com>
2023-11-20 14:28:22 +0100

    Add default environment settings

--- scripts/grafana-instrument.sh
+++ scripts/grafana-instrument.sh
@@ -8,4 +8,7 @@ if [ ! -z "$GRAFANA_CLOUD_INSTANCE_ID" -a ! -z "$GRAFANA_CLOUD_ZONE" -a ! -z "$G
   export OTEL_EXPORTER_OTLP_HEADERS="Authorization=Basic $(echo -n $GRAFANA_INSTANCE_ID:$GRAFANA_CLOUD_API_KEY| base64 -w 0)"
 fi
 
+export OTEL_DOTNET_AUTO_FAIL_FAST_ENABLED=true
+export OTEL_SEMCONV_STABILITY_OPT_IN=http
+
 "$OTEL_DOTNET_AUTO_HOME/instrument.sh" $@
